6529f5af081c6f39d6ce94819d8da0f09cc7ef2d
WCVFW <wcvfw2019@gmail.com>
2025-06-09 16:03:52 +0530

    updated

--- wcvfw/src/app/app.ts
+++ wcvfw/src/app/app.ts
@@ -4,9 +4,14 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './HTML/app.component.html',
+  styleUrls: ['./CSS/app.component.css'],
 })
 export class App {
-  protected title = 'we can voice for woman';
+  protected title = 'We Can Voice for Women';
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
